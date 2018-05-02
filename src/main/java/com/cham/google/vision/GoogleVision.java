a3bc8fcb38e7441a02f95119f79e29b47226c788
cwijayasundara <cwijayasundara@gmail.com>
2018-05-02 15:17:30 +0100

    First commit

---
+++ src/main/java/com/cham/google/vision/GoogleVision.java
@@ -0,0 +1,53 @@
+package com.cham.google.vision;
+import com.google.cloud.vision.v1.*;
+import com.google.protobuf.ByteString;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class GoogleVision {
+
+    private final static String fileLocation = "src/main/resources/galaxy1.jpg";
+
+    public static void main(String... args) throws Exception {
+
+        // Instantiates a client
+        try (ImageAnnotatorClient vision = ImageAnnotatorClient.create()) {
+
+            // Reads the image file into memory
+            Path path = Paths.get(fileLocation);
+            byte[] data = Files.readAllBytes(path);
+            ByteString imgBytes = ByteString.copyFrom(data);
+
+            // Builds the image annotation request
+            List<AnnotateImageRequest> requests = new ArrayList<>();
+            Image img = Image.newBuilder().setContent(imgBytes).build();
+            Feature feat = Feature.newBuilder().setType(Feature.Type.LABEL_DETECTION).build();
+            AnnotateImageRequest request = AnnotateImageRequest.newBuilder()
+                    .addFeatures(feat)
+                    .setImage(img)
+                    .build();
+            requests.add(request);
+
+            // Performs label detection on the image file
+            BatchAnnotateImagesResponse response = vision.batchAnnotateImages(requests);
+
+            List<AnnotateImageResponse> responses = response.getResponsesList();
+
+            for (AnnotateImageResponse res : responses) {
+                if (res.hasError()) {
+                    System.out.printf("Error: %s\n", res.getError().getMessage());
+                    return;
+                }
+
+                for (EntityAnnotation annotation : res.getLabelAnnotationsList()) {
+                    annotation.getAllFields().forEach((k, v) ->
+                            System.out.printf("%s : %s\n", k, v.toString()));
+                }
+            }
+        }
+    }
+}
